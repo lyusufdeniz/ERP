27ebf72c4bd74fd41328c59f66d49bfaa4c2d136
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2024-09-07 15:08:14 +0300

    import dashboard theme

--- ERPClient/src/app/components/layouts/layouts.component.ts
+++ ERPClient/src/app/components/layouts/layouts.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-layouts',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,SidebarComponent,FooterComponent],
   templateUrl: './layouts.component.html',
   styleUrl: './layouts.component.css'
 })
